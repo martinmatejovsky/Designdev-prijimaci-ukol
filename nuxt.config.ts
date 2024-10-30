bb2a8d7971be066bb6685a89b15e2a1497ef90e7
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-10-30 16:44:15 +0100

    Reinstalled Bootstrap with appropriate sass module version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['bootstrap/dist/css/bootstrap.min.css']
+  css: [
+    '~/assets/scss/main.scss',
+  ],
 })
