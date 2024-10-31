04e8249e840154996541d84ec7ca12814d5d30f0
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-10-31 13:28:56 +0100

    New page Index

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import ProductCard from "../components/ProductCard.vue";
+import {type Product} from "assets/interfaces";
+
+const { data: products } = await useFetch<Product[]>('/api/products')
+</script>
+
+<template>
+  <header>
+    <h1>The Fab 4</h1>
+    <nav></nav>
+  </header>
+
+  <main>
+    <div class="row">
+      <div v-for="product in products" :key="product.id" class="col-sm-6 col-lg-4 col-xl-3">
+        <ProductCard :product="product" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
