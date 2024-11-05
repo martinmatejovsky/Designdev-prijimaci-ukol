c81d40f2a39baa0703917ec29b737a3e888fc3fd
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-11-05 09:38:39 +0100

    nuxt server optimization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  ssr: false,
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: [
-    '~/assets/scss/main.scss',
-  ],
+  css: ["~/assets/scss/main.scss"],
   pages: false,
-})
+});
